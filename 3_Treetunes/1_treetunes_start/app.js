aca9b7effe806820cf7f624d2260d07441a60262
Jennifer Varahala <Jennifer Varahala>
2016-10-14 14:21:52 -0400

    added htmlString and set up songs for playlist

---
+++ 3_Treetunes/1_treetunes_start/app.js
@@ -0,0 +1,7 @@
+var playlist = new Playlist();
+
+var hereComesThesun = new Song("Here Comes the Sun", "The Beatles, "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves" , "3:43");
+
+playlist.add(hereComesThesun);
+platlist.add(walkingOnSunshine);
