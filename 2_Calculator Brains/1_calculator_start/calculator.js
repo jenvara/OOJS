fb723d13d210ed922cb76237f1ef2cc77fb557dd
Jennifer Varahala <Jennifer Varahala>
2016-10-14 11:05:02 -0400

    Modified Calculator with return this Method

--- 2_Calculator Brains/1_calculator_start/calculator.js
+++ 2_Calculator Brains/1_calculator_start/calculator.js
@@ -1,12 +1,12 @@
 var calculator = {
 		sum: 0,
 		add: function(value) {
-
+			this.sum += value;
     },
-    clear: function() {
-
+		clear: function() {
+			this.sum = 0;
     }, 
-    equals: function() {
-
+		equals: function() {
+			return this.sum;
     }
 }
