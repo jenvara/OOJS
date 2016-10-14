27708e0de38aec62a6a9d285c01ca169bddfcf9a
Jennifer Varahala <Jennifer Varahala>
2016-10-14 14:54:05 -0400

    Making the UI work on the buttons

--- 3_Treetunes/1_treetunes_start/app.js
+++ 3_Treetunes/1_treetunes_start/app.js
@@ -9,3 +9,20 @@ playlist.add(walkingOnSunshine);
 var playlistElement = document.getElementById("playlist");
 
 playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play");
+playButton.onlick = function() {
+	playlist.play();
+	playlist.renderInElement(playlistElement);
+}
+var nextButton = document.getElementById("next");
+nextButton.onlick = function() {
+	playlist.next();
+	playlist.renderInElement(playlistElement);
+}
+var stopButton = document.getElementById("stop");
+stopButton.onlick = function() {
+	playlist.stop();
+	playlist.renderInElement(playlistElement);
+}
+
