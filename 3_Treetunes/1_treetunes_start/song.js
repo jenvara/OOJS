598b52be06fcd87495108dd4d5e24b3fc35d20d0
Jennifer Varahala <Jennifer Varahala>
2016-10-14 13:56:41 -0400

    Added JS code for songs on playlist

--- 3_Treetunes/1_treetunes_start/song.js
+++ 3_Treetunes/1_treetunes_start/song.js
@@ -1,13 +1,17 @@
-function Song() {
-
+function Song(title, artist, duration) {
+	this.title = title;
+	this.artist = artist;
+	this.duration = duration;
+	this.isPlaying = false;
+	
 }
 
 Song.prototype.play = function() {
-
+	this.isPlaying = true;
 };
 
 Song.prototype.stop = function() {
-
+	this.isPlaying = false;
 };
 
 Song.prototype.toHTML = function() {
